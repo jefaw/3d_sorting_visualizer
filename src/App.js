d6cb41b79dd339725b4c2456a1e3673f6e04ee56
Jeffrey Wang <jwan2369@uwo.ca>
2022-06-10 16:27:57 -0400

    new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Bar from './components/Bar.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//css
+import "./App.css";
+
+class App extends Component {
+    state = {
+        array: [],
+        arraySteps: [],
+        colorKey: [],
+        colorSteps: [],
+        currentStep: 0,
+        count: 10,
+        delay: 100,
+        algorithm: '',
+        timeouts: [],
+
+    };
+    componentDidMount() {//runs after component mounted
+        this.generateRandomArray();
+    }
+    generateRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    generateRandomArray = () => {
+        const count = this.state.count;
+        const temp = [];
+
+        for (let i = 0; i < count; i++) {
+            temp.push(this.generateRandomNumber(20, 100));
+        }
+        console.log(temp);
+        this.setState({
+            array: temp,
+            arraySteps: [temp]
+        })
+    };
+    render() {
+        let bars = this.state.array.map((value, index) => {
+            return (
+                <Bar key={index} index={index} length={value} color={this.state.colorKey[index]} />
+            );
+        })
+        return (
+            <div className="App">
+                {bars}
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Bar.js
@@ -0,0 +1,16 @@
+import './Bar.css'
+
+function Bar({index, length, color}) {
+
+    const barStyle={
+        height: length
+    }
+    return (
+        <>
+            <div className="bar" style={barStyle}>
+
+            </div>
+        </>
+    )
+}
+export default Bar;
